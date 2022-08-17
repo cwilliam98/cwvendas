342b34eb2aaa53ac762026a69139647ed5af2af3
Cristian William Albrecht <cristian.albrecht@compasso.com.br>
2022-08-17 15:31:38 -0300

    Project created and database

---
+++ src/main/java/com/cwilliam/cwvendas/CwVendasApplication.java
@@ -0,0 +1,13 @@
+package com.cwilliam.cwvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwilliam/cwvendas/CwvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwilliam.cwvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
