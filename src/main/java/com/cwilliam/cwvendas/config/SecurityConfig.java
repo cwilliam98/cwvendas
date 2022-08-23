ebd969110cbc8a59585d0d61c31f25b0ba456ef6
cwilliam98 <cristianwillian174@gmail.com>
2022-08-23 14:42:11 -0300

    Api test

--- src/main/java/com/cwilliam/cwvendas/config/SecurityConfig.java
+++ src/main/java/com/cwilliam/cwvendas/config/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.cwilliam.cwvendas.config;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
