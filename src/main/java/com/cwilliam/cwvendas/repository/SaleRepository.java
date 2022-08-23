beb8e9cc9b1ecd3a12a7b51182549f3cccd5b6df
cwilliam98 <cristianwillian174@gmail.com>
2022-08-23 16:10:45 -0300

    Date select

--- src/main/java/com/cwilliam/cwvendas/repository/SaleRepository.java
+++ src/main/java/com/cwilliam/cwvendas/repository/SaleRepository.java
@@ -1,11 +1,18 @@
 package com.cwilliam.cwvendas.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cwilliam.cwvendas.entities.Sale;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-    
+
+  @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :minDate AND :maxDate ORDER BY obj.amount DESC")
+  Page<Sale> findSales(LocalDate minDate, LocalDate maxDate, Pageable pageable);
 }
